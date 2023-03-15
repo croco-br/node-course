fcd28a5c8a0d8533d376540ac773d433876ffb3d
croco <adrianomxcx@gmail.com>
2023-03-15 15:47:55 -0300

    add path, routes and html views

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const http = require('http');
-const routes = require('./routes.js')
+const express = require('express');
+const path = require('path')
+const rootDir = require('./util/path')
+const app = express()
+const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const server = http.createServer(routes.handler)  
+app.use(bodyParser.urlencoded({ extended: false }))
 
-console.log(routes.text)
-server.listen(3000) 
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+});
+
+app.listen(3000)
