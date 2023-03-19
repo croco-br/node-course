62b4ce20ac77069d1e21f5a6d3320c8d09e0ad6f
croco <adrianomxcx@gmail.com>
2023-03-19 09:22:08 -0300

    feat: add ejs

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
 
 const app = express()
 
-//hbs
-app.engine(
-    "hbs",
-    expressHbs.engine({
-      defaultLayout: "main-layout.hbs",
-      layoutsDir: "views/layouts/",
-      extname: 'hbs'
-    })
-  );
-
-//pug setup
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 
@@ -30,7 +18,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use('/', (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page not found' })
+    res.status(404).render('404', { pageTitle: 'Page not found', path: '/' })
 });
 
 app.listen(3000)
