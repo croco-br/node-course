aa3488585281aaabdd8c16788f3c3f6acea3e5e2
croco <adrianomxcx@gmail.com>
2023-03-19 07:43:27 -0300

    feat: added pug and views files

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require('express');
 const path = require('path')
 const rootDir = require('./util/path')
 const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const bodyParser = require('body-parser')
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404')
 });
 
 app.listen(3000)
