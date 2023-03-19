298bf34c4b807c2b4e4d29ed76cd010eb3358d01
croco <adrianomxcx@gmail.com>
2023-03-19 08:53:18 -0300

    feat: add handlebars view engine

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
 const path = require('path')
-const rootDir = require('./util/path')
+const express = require('express');
+const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
+
 const app = express()
 
-app.set('view engine', 'pug')
+//hbs
+app.engine(
+    "hbs",
+    expressHbs.engine({
+      defaultLayout: "main-layout.hbs",
+      layoutsDir: "views/layouts/",
+      extname: 'hbs'
+    })
+  );
+
+//pug setup
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-const bodyParser = require('body-parser')
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
@@ -16,9 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
-
 app.use('/', (req, res, next) => {
-    res.status(404).render('404')
+    res.status(404).render('404', { pageTitle: 'Page not found' })
 });
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,12 @@ const products = [];
 
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product'})
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        productCSS: true,
+        activeAddProduct: true
+    })
 });
 
 router.post('/add-product', (req, res, next) => {
